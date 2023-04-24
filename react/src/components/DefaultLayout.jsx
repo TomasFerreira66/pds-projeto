62c0ab965ffcb3ca24f67c4bdb8925d19442fa26
TomasFerreira66 <tomasff2002@gmail.com>
2023-04-24 14:18:23 +0100

    first commit

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,60 @@
+import {Link, Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider";
+import axiosClient from "../axios-client.js";
+import {useEffect} from "react";
+
+export default function DefaultLayout() {
+  const {user, token, setUser, setToken, notification} = useStateContext();
+
+  if (!token) {
+    return <Navigate to="/login"/>
+  }
+
+  const onLogout = ev => {
+    ev.preventDefault()
+
+    axiosClient.post('/logout')
+      .then(() => {
+        setUser({})
+        setToken(null)
+      })
+  }
+
+  useEffect(() => {
+    axiosClient.get('/user')
+      .then(({data}) => {
+         setUser(data)
+      })
+  }, [])
+
+  return (
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Utilizadores</Link>
+        <Link to="/marcacoes">Marcações</Link>
+        <Link to="/contactos">Contactos</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>
+            
+          </div>
+
+          <div>
+            {user.name} &nbsp; &nbsp;
+            <a onClick={onLogout} className="btn-logout" href="#">Terminar sessão</a>
+          </div>
+        </header>
+        <main>
+          <Outlet/>
+        </main>
+        {notification &&
+          <div className="notification">
+            {notification}
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
