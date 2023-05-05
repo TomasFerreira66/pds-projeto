d4f3ed9517fe8c8116d23e3a7296596e39325268
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-05-05 16:12:05 +0100

    form especialidade

--- react/src/viewsAdmin/UserForm.jsx
+++ react/src/viewsAdmin/UserForm.jsx
@@ -86,7 +86,7 @@ export default function UserForm() {
           <input value={user.email} onChange={ev => setUser({...user, email: ev.target.value})} placeholder="Email"/>
           <input type="password" onChange={ev => setUser({...user, password: ev.target.value})} placeholder="Palavra-passe"/>
           <input type="password" onChange={ev => setUser({...user, password_confirmation: ev.target.value})} placeholder="Confirmar palavra-passe"/>
-          <select name="especialidade" value={user.especialidade} onChange={ev => setUser({...user, especialidade: ev.target.value})}>
+          <select className="dropdown-menu" name="especialidade" value={user.especialidade} onChange={ev => setUser({...user, especialidade: ev.target.value})}>
             <option>Especialidade</option>
             <option value="Corte">Corte</option>
             <option value="Barba">Barba</option>
@@ -94,7 +94,6 @@ export default function UserForm() {
           </select>
           <br></br><br></br>
           <button className="btn">Adicionar barbeiro</button>
-
         </form>
         
         )}
