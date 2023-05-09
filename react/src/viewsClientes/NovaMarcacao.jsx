19b23b8950f95165cd60a88ffea1675cf545992c
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-05-09 21:57:47 +0100

    Update NovaMarcacao.jsx

--- react/src/viewsClientes/NovaMarcacao.jsx
+++ react/src/viewsClientes/NovaMarcacao.jsx
@@ -9,6 +9,7 @@ import "react-datepicker/dist/react-datepicker.css";
 export default function NovaMarcacao() {
   const navigate = useNavigate();
   const {setNotification} = useStateContext()
+  const [errors, setErrors] = useState(null)
   const [barbeiros, setBarbeiros] = useState([]);
   const [especialidades, setEspecialidades] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -95,57 +96,74 @@ export default function NovaMarcacao() {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <div className='card animated fadeInDown' style={{ marginLeft: '100px', marginRight: '100px' }}>
-        <h2>Agendar uma marcação</h2>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
-          <br /><br />
-          <h4 style={{ marginBottom: '10px' }}>Selecionar serviço:</h4>
-          <select className='dropdown-servico' style={{ marginTop: '20px' }} onChange={(ev) => {
-            setMarcacao({ ...marcacao, servico: ev.target.value });
-            getBarbeirosEspecialidade(ev.target.value);
-          }}>
-            <option value="">Escolha uma opção</option>
-            {especialidades.map((especialidade, index) => (
-              <option value={especialidade} key={index}>
-                {especialidade}
-              </option>
-            ))}
-          </select>
+    <>
+    <div className='cardd animated fadeInDown'>
+      {loading && (
+        <div className='text-center'>
+          Loading...
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
-          <h4 style={{ marginBottom: '10px' }}>Selecionar barbeiro:</h4>
-          <select className='dropdown-barbeiro' style={{ marginTop: '20px' }} onChange={(ev) => setMarcacao({ ...marcacao, idBarbeiro: ev.target.value })}>
-            <option value=''>Escolha uma opção</option>
-            {barbeiros.map((barbeiro, index) => (
-              <option value={barbeiro.id} key={index}>
-                {barbeiro.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px', textAlign: 'center' }}>
-          <h4 style={{ marginBottom: '10px' }}>Selecionar data e hora:</h4>
-          <DatePicker 
-          style={{ width:'300px' }}
-          className='dropdown-horario'
-          selected={dataHoraSelecionada}
-          onChange={(date) => mudarData(date)}
-          showTimeSelect
-          timeIntervals={30}
-          timeFormat="HH:mm"
-          dateFormat="dd/MM/yyyy HH:mm"
-          minDate={new Date()}
-          maxDate={new Date('2030-12-31')}
-          minTime={new Date().setHours(9, 0)}
-          maxTime={new Date().setHours(17, 30)}
-        />
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
-          <button className='btn-marcacao'>Confirmar marcação</button>
-        </div>
-      </div>
-    </form>
-  );
-  
-    }
+      )}
+      {errors &&
+      <div className='alert'>
+        {Object.keys(errors).map(key => (
+          <p key={key}>{errors[key][0]}</p>
+          ))}
+          </div>
+          }
+        {!loading && (
+          <form onSubmit={onSubmit}>
+          <div className='card animated fadeInDown' style={{ marginLeft: '100px', marginRight: '100px' }}>
+            <h2>Agendar uma marcação</h2>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
+              <br /><br />
+              <h4 style={{ marginBottom: '10px' }}>Selecionar serviço:</h4>
+              <select className='dropdown-servico' style={{ marginTop: '20px' }} onChange={(ev) => {
+                setMarcacao({ ...marcacao, servico: ev.target.value });
+                getBarbeirosEspecialidade(ev.target.value);
+              }}>
+                <option value="">Escolha uma opção</option>
+                {especialidades.map((especialidade, index) => (
+                  <option value={especialidade} key={index}>
+                    {especialidade}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
+              <h4 style={{ marginBottom: '10px' }}>Selecionar barbeiro:</h4>
+              <select className='dropdown-barbeiro' style={{ marginTop: '20px' }} onChange={(ev) => setMarcacao({ ...marcacao, idBarbeiro: ev.target.value })}>
+                <option value=''>Escolha uma opção</option>
+                {barbeiros.map((barbeiro, index) => (
+                  <option value={barbeiro.id} key={index}>
+                    {barbeiro.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px', textAlign: 'center' }}>
+              <h4 style={{ marginBottom: '10px' }}>Selecionar data e hora:</h4>
+              <DatePicker 
+              style={{ width:'300px' }}
+              className='dropdown-horario'
+              selected={dataHoraSelecionada}
+              onChange={(date) => mudarData(date)}
+              showTimeSelect
+              timeIntervals={30}
+              timeFormat="HH:mm"
+              dateFormat="dd/MM/yyyy HH:mm"
+              minDate={new Date()}
+              maxDate={new Date('2030-12-31')}
+              minTime={new Date().setHours(9, 0)}
+              maxTime={new Date().setHours(17, 30)}
+            />
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
+              <button className='btn-marcacao'>Confirmar marcação</button>
+            </div>
+          </div>
+        </form>
+        )}
+    </div>
+    </>
+  )
+}
