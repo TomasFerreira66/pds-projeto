b86e45e0cb90fd757a4d1f4ec2e59f318d7dd5ad
TomasFerreira66 <tomasff2002@gmail.com>
2023-05-09 19:36:33 +0100

    IDCLIENTE FEITO

--- app/Http/Requests/StoreMarcacaoRequest.php
+++ app/Http/Requests/StoreMarcacaoRequest.php
@@ -25,7 +25,7 @@ class StoreMarcacaoRequest extends FormRequest
             'servico' => 'required|string|max:55',
             //'data' => 'required|unique:marcacoes,data',
             'idBarbeiro' => 'required',
-            //'idCliente' => 'required'
+            'idCliente' => 'required'
         ];
     }
 }
