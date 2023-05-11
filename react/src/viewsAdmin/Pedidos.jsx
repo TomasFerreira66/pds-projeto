892344d47805238626929050aff366f95a62cc4f
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-05-11 19:05:36 +0100

    Update Pedidos.jsx

--- react/src/viewsAdmin/Pedidos.jsx
+++ react/src/viewsAdmin/Pedidos.jsx
@@ -1,7 +1,7 @@
 export default function Pedidos(){
     return (
         <div style={{ marginLeft: "100px", marginRight: "100px" }}>
-          <h2>Produtos</h2>
+          <h2>Pedidos</h2>
           <br />
           <div className="card-container" style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "10px" }}>
           </div>
