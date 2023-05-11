71fd976cbd17b6a814f86eca30507107978f68f0
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-05-11 22:41:31 +0100

    Update carrinho.jsx

--- react/src/viewsClientes/carrinho.jsx
+++ react/src/viewsClientes/carrinho.jsx
@@ -1,9 +1,10 @@
 export default function Carrinho(){
     return (
-        <div className='card animated fadeInDown' style={{ marginLeft: '100px' , marginRight: '100px'}}>
-            <div style={{display: 'flex', justifyContent: "space-between", alignItems: "center"}}/>
-            <h2>Carrinho</h2>
-
+        <div style={{ marginLeft: "100px", marginRight: "100px" }}>
+          <h2>Carrinho de compras</h2>
+          <br />
+          <div className="card-container" style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "10px" }}>
+          </div>
         </div>
-    )
+      );  
 }
