4d95b8ddafc01ab0630fb6c7d9b8ea6da6968322
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-05-11 23:47:19 +0100

    slider

--- react/src/viewsClientes/PaginaInicialoriginal.jsx
+++ react/src/viewsClientes/PaginaInicialoriginal.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function PaginaInicialOriginal() {
-    return (
-      <div id="defaultLayout">
-        <aside>
-          <Link to="/paginainicial">Página Inicial</Link>
-          <Link to="/login">Marcações</Link>
-          <Link to="/login">Produtos</Link>
-          <Link to="/contactosguest">Contactos</Link>
-        </aside>
-        <div className="content">
+  return (
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/paginainicial">Página Inicial</Link>
+        <Link to="/login">Marcações</Link>
+        <Link to="/login">Produtos</Link>
+        <Link to="/contactosguest">Contactos</Link>
+      </aside>
+      <div className="content">
         <header>
           <div>
             <img
@@ -21,47 +23,64 @@ export default function PaginaInicialOriginal() {
             />
           </div>
           <Link to="/login">
-          <button className="btn-login">Iniciar sessão</button>
-        </Link>
-        </header>
-      <div className='card animated fadeInDown' style={{ marginLeft: '100px' , marginRight: '100px'}}>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', height: '10vh' }}>
-        <Link to={`/login`}>
-            <button className="btn-marcacao">FAZER MARCAÇÃO</button>
+            <button className="btn-login">Iniciar sessão</button>
           </Link>
-        </div>
-        <h3>Produtos</h3><br /><br />
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '30vh' }}>
-          <div className='card animated fadeInDown' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '45%' }}>
-            <img
-              src="../src/img/cabelo.png"
-              alt="cabelo"
-              className="imagem-cabelo"
-            />
-            <br />
-            <h4>CABELO</h4>
-            <br />
-            <Link to="/login">
-              <button className="btn-comprar">Comprar</button>
+        </header>
+        <div className='card animated fadeInDown' style={{ marginLeft: '100px', marginRight: '100px' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', height: '10vh' }}>
+            <Link to={`/login`}>
+              <button className="btn-marcacao">FAZER MARCAÇÃO</button>
             </Link>
           </div>
-          <div className='card animated fadeInDown' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '45%' }}>
-            <img
-              src="../src/img/barba.png"
-              alt="barba"
-              className="imagem-barba"
-            />
-            <br />
-            <h4>BARBA</h4>
-            <br />
-            <Link to="/login">
-              <button className="btn-comprar">Comprar</button>
-            </Link>
+          <h3>Produtos</h3><br /><br />
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '30vh' }}>
+            <div className='card animated fadeInDown' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '45%' }}>
+              <img
+                src="../src/img/cabelo.png"
+                alt="cabelo"
+                className="imagem-cabelo"
+              />
+              <br />
+              <h4>CABELO</h4>
+              <br />
+              <Link to="/login">
+                <button className="btn-comprar">Comprar</button>
+              </Link>
+            </div>
+            <div className='card animated fadeInDown' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '45%' }}>
+              <img
+                src="../src/img/barba.png"
+                alt="barba"
+                className="imagem-barba"
+              />
+              <br />
+              <h4>BARBA</h4>
+              <br />
+              <Link to="/login">
+                <button className="btn-comprar">Comprar</button>
+              </Link>
+            </div>
+          </div>
+          <br /><br />
+          <div className="slide-container card animated fadeInDown" style={{ maxWidth: "900px", margin: "0 auto" }}>
+            <Carousel autoPlay showThumbs={false} infiniteLoop>
+              <div>
+                <img style={{ height: "450px", borderRadius: "10px" }} src="../src/img/slide1.png" alt="slide 1" />
+              </div>
+              <div>
+                <img style={{ height: "450px", borderRadius: "10px" }} src="../src/img/slide2.png" alt="slide 2" />
+              </div>
+              <div>
+                <img style={{ height: "450px", borderRadius: "10px" }} src="../src/img/slide3.png" alt="slide 3" />
+                </div>
+                <div>
+                <img style={{ height: "450px", borderRadius: "10px" }} src="../src/img/slide4.png" alt="slide 1" />
+              </div>
+            </Carousel>
           </div>
         </div>
-        <br />
       </div>
-      </div>
-      </div>
-    );
-  }
+    </div>
+  );
+}
+
