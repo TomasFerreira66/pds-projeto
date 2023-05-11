57c381b451942b085917ed7d8ae9e7a29df589d6
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-05-11 19:05:05 +0100

    Update Pedidos.jsx

--- react/src/viewsAdmin/Pedidos.jsx
+++ react/src/viewsAdmin/Pedidos.jsx
@@ -1,8 +1,10 @@
 export default function Pedidos(){
     return (
-        <div className='card animated fadeInDown' style={{ marginLeft: '100px' , marginRight: '100px'}}>
-            <div style={{display: 'flex', justifyContent: "space-between", alignItems: "center"}}/>
-            <h2>Pedidos</h2>
+        <div style={{ marginLeft: "100px", marginRight: "100px" }}>
+          <h2>Produtos</h2>
+          <br />
+          <div className="card-container" style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "10px" }}>
+          </div>
         </div>
-    )
+      );  
 }
