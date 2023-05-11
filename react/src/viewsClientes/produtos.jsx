883fa0412c9854eec65676f3a2cc0139db087024
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-05-11 18:58:13 +0100

    Update produtos.jsx

--- react/src/viewsClientes/produtos.jsx
+++ react/src/viewsClientes/produtos.jsx
@@ -40,45 +40,36 @@ export default function ProdutosCliente() {
     <div style={{ marginLeft: "100px", marginRight: "100px" }}>
       <h2>Produtos</h2>
       <br />
-      <div className="card animated fadeInDown" style={{ display: "grid", gridTemplateColumns: "50% 50%", gap: "10px" }}>
-        {loading && (
-          <div className="text-center">
-            Loading...
-          </div>
-        )}
-        {!loading && (
-          <>
-            {produtos.map(produto => (
-              <div key={produto.id} style={{ display: "grid", gridTemplateRows: "1fr auto auto", padding: "10px", borderRadius: "10px" }}>
-                <div>{produto.nome}</div>
-                <div>{produto.descricao}</div>
-                <div>{`${produto.preco} €`}</div>
-                <div>Quantidade em stock: {produto.quantidade}</div>
-                <div>
-                  <div style={{ display: "flex" }}>
-                    <input
-                      style={{ width: 70, height: 50, marginRight: "10px"}}
-                      type="number"
-                      min="1"
-                      max={produto.quantidade}
-                      onChange={e => {
-                        const quantidade = parseInt(e.target.value);
-                        if (quantidade > produto.quantidade) {
-                          setNotification(`Quantidade insuficiente (${produto.quantidade} disponíveis)`);
-                        } else {
-                          handleAddToCart(produto, quantidade);
-                        }
-                      }}
-                    />
-                    <button style={{width: 120, height: 50}} className="btn-login" onClick={() => handleAddToCart(produto, 1)}>Adicionar</button>
-                  
-                  </div>
-                </div>
+      <div className="card-container" style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "10px" }}>
+        {produtos.map(produto => (
+          <div key={produto.id} className="card animated fadeInDown" style={{ display: "grid", gridTemplateRows: "1fr auto auto", padding: "10px", borderRadius: "10px" }}>
+            <div>{produto.nome}</div>
+            <div>{produto.descricao}</div>
+            <div>{`${produto.preco} €`}</div>
+            <div>Quantidade em stock: {produto.quantidade}</div>
+            <div>
+              <div style={{ display: "flex" }}>
+                <input
+                  style={{ width: 70, height: 50, marginRight: "10px"}}
+                  type="number"
+                  min="1"
+                  max={produto.quantidade}
+                  onChange={e => {
+                    const quantidade = parseInt(e.target.value);
+                    if (quantidade > produto.quantidade) {
+                      setNotification(`Quantidade insuficiente (${produto.quantidade} disponíveis)`);
+                    } else {
+                      handleAddToCart(produto, quantidade);
+                    }
+                  }}
+                />
+                <button style={{width: 120, height: 50}} className="btn-login" onClick={() => handleAddToCart(produto, 1)}>Adicionar</button>
               </div>
-            ))}
-          </>
-        )}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
+   
 }
