b89e28910e0764e98c91e921ffdcd33b4894c337
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-05-15 13:37:35 +0100

    Update Stock.jsx

--- react/src/viewsAdmin/Stock.jsx
+++ react/src/viewsAdmin/Stock.jsx
@@ -6,7 +6,7 @@ import { useStateContext } from "../contexts/ContextProvider.jsx";
 export default function Stock() {
   const [produtos, setProdutos] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [quantidadeInput, setQuantidadeInput] = useState('');
+  const [quantidades, setQuantidades] = useState({});
 
   useEffect(() => {
     getProdutos();
@@ -26,39 +26,44 @@ export default function Stock() {
       });
   }
 
-  const handleQuantidadeChange = (event) => {
-    setQuantidadeInput(event.target.value);
+  const handleQuantidadeChange = (event, produtoId) => {
+    const value = event.target.value;
+    setQuantidades(prevQuantidades => ({
+      ...prevQuantidades,
+      [produtoId]: value
+    }));
   };
 
   const handleAtualizarQuantidade = (produtoId) => {
-    const quantidade = parseInt(quantidadeInput);
-      const url = `/produtos/${produtoId}`;
-      axiosClient.get(url)
-        .then(({ data }) => {
-          const quantidadeExistente = data.data.quantidade;
-          const novaQuantidade = quantidadeExistente + quantidade;
-          axiosClient.patch(url, { quantidade: novaQuantidade })
-            .then(() => {
-              const updatedProdutos = produtos.map(produto => {
-                if (produto.id === produtoId) {
-                  return {
-                    ...produto,
-                    quantidade: novaQuantidade
-                  };
-                }
-                return produto;
-              });
-              setProdutos(updatedProdutos);
-              setQuantidadeInput('');
-            })
-            
-        })
+    const quantidade = parseInt(quantidades[produtoId]);
+    const url = `/produtos/${produtoId}`;
+    axiosClient.get(url)
+      .then(({ data }) => {
+        const quantidadeExistente = data.data.quantidade;
+        const novaQuantidade = quantidadeExistente + quantidade;
+        axiosClient.patch(url, { quantidade: novaQuantidade })
+          .then(() => {
+            const updatedProdutos = produtos.map(produto => {
+              if (produto.id === produtoId) {
+                return {
+                  ...produto,
+                  quantidade: novaQuantidade
+                };
+              }
+              return produto;
+            });
+            setProdutos(updatedProdutos);
+            setQuantidades(prevQuantidades => ({
+              ...prevQuantidades,
+              [produtoId]: '' // Limpa a quantidade do produto após atualização
+            }));
+          })
+      });
   };
-  
 
   return (
-    <div style={{ marginLeft: '100px' , marginRight: '100px'}}>
-      <div style={{display: 'flex', justifyContent: "space-between", alignItems: "center"}}>
+    <div style={{ marginLeft: '100px', marginRight: '100px' }}>
+      <div style={{ display: 'flex', justifyContent: "space-between", alignItems: "center" }}>
         <h2>Stock</h2>
         <div>
         </div>
@@ -66,50 +71,51 @@ export default function Stock() {
       <div className="card animated fadeInDown">
         <table>
           <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Descrição</th>
-            <th>Preço</th>
-            <th>Quantidade</th>
-            <th>Tipo</th> 
-            <th>Ações</th>
-          </tr>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Descrição</th>
+              <th>Preço</th>
+              <th>Quantidade</th>
+              <th>Tipo</th>
+              <th>Ações</th>
+            </tr>
           </thead>
           {loading &&
             <tbody>
-            <tr>
-              <td colSpan="6" className="text-center">
-                Loading...
-              </td>
-            </tr>
+              <tr>
+                <td colSpan="6" className="text-center">
+                  Loading...
+                </td>
+              </tr>
             </tbody>
           }
           {!loading &&
             <tbody>
-            {produtos.map(produto => (
+              {produtos.map(produto => (
                 <tr key={produto.id}>
-                  <td>{produto.id}</td>
-                  <td>{produto.nome}</td>
-                  <td>{produto.descricao}</td>
-                  <td>{`${produto.preco} €`}</td>
-                  <td>{produto.quantidade}</td>
-                  <td>{produto.tipo}</td>
-                  <td>
-                    <input
-                        style={{ width: 70, height: 20, marginRight: 10}}
-                      type="number"
-                      min="0"
-                      value={quantidadeInput}
-                      onChange={handleQuantidadeChange}
-                    />
-                    <button onClick={() => handleAtualizarQuantidade(produto.id)} className="btn-edit">Adicionar</button>
-                  </td>
-              </tr>
-            ))}
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.descricao}</td>
+                <td>{`${produto.preco} €`}</td>
+                <td>{produto.quantidade}</td>
+                <td>{produto.tipo}</td>
+                <td style={{ display: 'flex', alignItems: 'center' }}>
+                  <input stl
+                    style={{ width: 70, height: 20, marginRight: 10, marginTop: 14 }}
+                    type="number"
+                    min="0"
+                    value={quantidades[produto.id] || ''}
+                    onChange={(event) => handleQuantidadeChange(event, produto.id)}
+                  />
+                  <button onClick={() => handleAtualizarQuantidade(produto.id)} className="btn-edit">Adicionar</button>
+                </td>
+              </tr>              
+              ))}
             </tbody>
           }
         </table>
       </div>
     </div>
-  )}
+  );
+}
