c0ce600febcfe8bcd70e099e76c35c7f41aa553f
diogomigas78 <diogomigas78@gmail.com>
2023-05-17 20:48:33 +0100

    dsa

--- react/src/viewsClientes/produtos.jsx
+++ react/src/viewsClientes/produtos.jsx
@@ -12,23 +12,24 @@ export default function Produtos() {
   const [searchTerm, setSearchTerm] = useState('');
   const [sortOrder, setSortOrder] = useState("desc");
   const navigate = useNavigate();
-  const {setNotification} = useStateContext();
+  const { setNotification } = useStateContext();
   const [errors, setErrors] = useState(null);
-  const {id} = useParams();
+  const { id } = useParams();
+  const [errorMessage, setErrorMessage] = useState('');
+  const { setAlert } = useStateContext();
 
-    
   const [produtoEscolhido, setProdutoEscolhido] = useState({
-    id:null,
-    idProduto:'',
+    id: null,
+    idProduto: '',
     idCliente: localStorage.getItem('userId'),
-    quantidadePedida: '', 
-    
-  })
+    quantidadePedida: '',
+    quantidade: 0,
+  });
 
   useEffect(() => {
     getProdutos();
-  }, [])
-  
+  }, []);
+
   const handleFilterChange = (event) => {
     const newFilter = event.target.value;
     setLoading(true);
@@ -37,28 +38,34 @@ export default function Produtos() {
       setLoading(false);
       setProdutos(data);
     });
-  }
+  };
+
   const handleSortChange = (event) => {
     setSortOrder(event.target.value);
     sortProdutos(event.target.value);
   };
-  
-  
+
   const onSubmit = (ev, produtoId) => {
     ev.preventDefault();
     const updatedProdutoEscolhido = {
       ...produtoEscolhido,
       idProduto: produtoId.toString(),
     };
-  
-    console.log(updatedProdutoEscolhido);
-  
-    axiosClient.post('/carrinhos', updatedProdutoEscolhido)
+
+    // Verificar se a quantidade selecionada é maior do que a quantidade em stock
+    if (parseInt(updatedProdutoEscolhido.quantidadePedida) > produtoEscolhido.quantidade) {
+      const quantidadeDisponivel = produtoEscolhido.quantidade;
+      setNotification(`Quantidade indisponível, quantidade em stock : ${quantidadeDisponivel}`);
+      return;
+    }
+
+    axiosClient
+      .post('/carrinhos', updatedProdutoEscolhido)
       .then(() => {
-        setNotification('Marcação criada com sucesso');
+        setNotification('Produto adicionado ao carrinho com sucesso');
         navigate('/produtos');
       })
-      .catch(err => {
+      .catch((err) => {
         const response = err.response;
         if (response && response.status === 422) {
           setErrors(response.data.errors);
@@ -72,7 +79,8 @@ export default function Produtos() {
     if (filtro !== 'Todos') {
       url += `?tipo=${filtro}`;
     }
-    axiosClient.get(url)
+    axiosClient
+      .get(url)
       .then(({ data }) => {
         setLoading(false);
         setProdutos(data.data);
@@ -81,7 +89,7 @@ export default function Produtos() {
         setLoading(false);
       });
   };
-  
+
   const sortProdutos = (order) => {
     let sortedProdutos = [...produtos];
     sortedProdutos.sort((a, b) => {
@@ -93,63 +101,74 @@ export default function Produtos() {
     });
     setProdutos(sortedProdutos);
   };
-  
 
   const handleSearch = (event) => {
     const term = event.target.value.toLowerCase();
     setSearchTerm(term);
   };
 
-  const filteredProdutos = produtos.filter(produto =>
-    produto.nome.toLowerCase().includes(searchTerm) ||
-     produto.descricao.toLowerCase().includes(searchTerm) ||
-    produto.tipo.toLowerCase().includes(searchTerm)
+  const filteredProdutos = produtos.filter(
+    (produto) =>
+      produto.nome.toLowerCase().includes(searchTerm) ||
+      produto.descricao.toLowerCase().includes(searchTerm) ||
+      produto.tipo.toLowerCase().includes(searchTerm)
   );
 
   return (
-    <div style={{ marginLeft: "100px", marginRight: "100px"}}>
+    <div style={{ marginLeft: "100px", marginRight: "100px" }}>
       <h2>Produtos</h2>
       <br />
       <div>
-      <div style={{ dkiisplay: 'flex', alignItems: 'center' }}>
-      <input type="text" placeholder="Pesquisar" value={searchTerm} onChange={handleSearch} style={{ marginRight: '10px',  marginBottom: '-15px' }} />
-        <select name="ordenar2" value={sortOrder} onChange={handleSortChange}>
-          <option value="asc">Preço: Mais baixo para mais alto</option>
-          <option value="desc">Preço: Mais alto para mais baixo</option>
-        </select>
-</div>
-
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            value={searchTerm}
+            onChange={handleSearch}
+            style={{ marginRight: '10px', marginBottom: '-15px' }}
+          />
+          <select name="ordenar2" value={sortOrder} onChange={handleSortChange}>
+            <option value="asc">Preço: Mais baixo para mais alto</option>
+            <option value="desc">Preço: Mais alto para mais baixo</option>
+          </select>
+        </div>
       </div>
       <br />
       <div className="card-container" style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "10px" }}>
-        {filteredProdutos.map(produto => (
-
-          <form onSubmit={(event) => onSubmit(event, produto.id)}>
-          <div id={produto.id} className="card animated fadeInDown key" style={{ display: "grid", gridTemplateRows: "1fr auto auto", padding: "10px", borderRadius: "10px" }}>
-          <div>{produto.nome}</div>
-          <div>{produto.descricao}</div>
-          <div>{`${produto.preco} €`}</div>
-          <div>Quantidade em stock: {produto.quantidade}</div>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <input className="quantidadeproduto"
-              style={{ width: 70, height: 53, marginRight: "10px", marginTop:9}}
-              type="number"
-              min="1"
-              max={produto.quantidade}
-              onChange={(ev) => setProdutoEscolhido({ ...produtoEscolhido, quantidadePedida:ev.target.value })}
-            />
-            <button
-              style={{ width: 200, height: 50 }}
-              className="btn-login"
-              onClick={(event) => onSubmit(event, produto.id)}
-            >
-              Adicionar ao carrinho
-            </button>
-        </div>
-        </div>
-           </form>
+        {filteredProdutos.map((produto) => (
+          <form key={produto.id} onSubmit={(event) => onSubmit(event, produto.id)}>
+            <div id={produto.id} className="card animated fadeInDown key" style={{ display: "grid", gridTemplateRows: "1fr auto auto", padding: "10px", borderRadius: "10px" }}>
+              <div>{produto.nome}</div>
+              <div>{produto.descricao}</div>
+              <div>{`${produto.preco} €`}</div>
+              <div>Quantidade em stock: {produto.quantidade}</div>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <input
+                  className="quantidadeproduto"
+                  style={{ width: 70, height: 53, marginRight: "10px", marginTop: 9 }}
+                  type="number"
+                  min="1"
+                  max={produto.quantidade}
+                  onChange={(ev) =>
+                    setProdutoEscolhido({
+                      ...produtoEscolhido,
+                      quantidadePedida: ev.target.value,
+                      quantidade: produto.quantidade,
+                    })
+                  }
+                />
+                <button
+                  style={{ width: 200, height: 50 }}
+                  className="btn-login"
+                  onClick={(event) => onSubmit(event, produto.id)}
+                >
+                  Adicionar ao carrinho
+                </button>
+              </div>
+            </div>
+          </form>
         ))}
-        </div>
-        </div>
-        );
-        };
+      </div>
+    </div>
+  );
+}
