636b87fb10aa101ed6738fcbf46415a3c9a47ca3
diogomigas78 <diogomigas78@gmail.com>
2023-05-23 21:51:48 +0100

    Revert "marcações"
    This reverts commit d93ee3c6b2716d8a55146abc571f87b26b093238.

--- react/src/viewsClientes/NovaMarcacao.jsx
+++ react/src/viewsClientes/NovaMarcacao.jsx
@@ -4,7 +4,8 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useStateContext } from '../contexts/ContextProvider';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import emailjs from 'emailjs-com';
+import { format } from 'date-fns';
 
 export default function NovaMarcacao() {
   const navigate = useNavigate();
@@ -23,14 +24,12 @@ export default function NovaMarcacao() {
     idBarbeiro: '',
     idCliente: id,
     estado: 'Ativo',
+    custo: 0,
   })
-
-  const [datasIndisponiveis, setDatasIndisponiveis] = useState([]);
-
+  
   useEffect(() => {
     getBarbeiros();
     getEspecialidades();
-    getDatasIndisponiveis();
   }, [])
 
   const mudarData = (ev) => {
@@ -75,25 +74,40 @@ export default function NovaMarcacao() {
       .catch(() => {
       })
   }
+  
+const onSubmit = ev => {
+  ev.preventDefault();
 
-  const onSubmit = ev => {
-    ev.preventDefault();
-    setMarcacao({ ...marcacao, data: new Date( document.querySelector('.dropdown-horario').value) });
-    setMarcacao(prevMarcacao => ({
-      ...prevMarcacao,
-      servico: document.querySelector('.dropdown-servico').value,
-      data:new Date( document.querySelector('.dropdown-horario').value),
-      estado: 'Indisponível',
-       
-    }));
+  axiosClient.post('/marcacaos', marcacao)
+    .then(() => {
+      setNotification('Marcação criada com sucesso')
+      navigate('/paginainicial')
 
-    console.log(marcacao);
+      axiosClient.get(`/users/${marcacao.idCliente}`)
+        .then(response => {
+          const clientEmail = response.data.email;
 
-    axiosClient.post('/marcacaos', marcacao)
-      .then(() => {
-        setNotification('Marcação criada com sucesso')
-        navigate('/paginainicial')
-      })
+          axiosClient.get(`/users/${marcacao.idBarbeiro}`)
+            .then(response => {
+              const nomeBarbeiro = response.data.name;
+
+              const templateParams = {
+                to_email: clientEmail, 
+                subject: "Confirmação de marcação",
+                message: `A sua marcação para o serviço ${marcacao.servico} com o barbeiro ${nomeBarbeiro} foi marcada para o dia ${format(marcacao.data, 'dd/MM/yyyy \'às\' HH:mm')}.`,
+              };
+
+              emailjs.send('service_hgpw1ul', 'template_849x1az', templateParams, '19c0R-gO8pAzmZ2sf')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+
+            }).catch(err => console.log(err)); // handle error here
+
+        }).catch(err => console.log(err)); // handle error here
+    })
       .catch(err => {
         const response = err.response;
         if (response && response.status === 422) {
@@ -101,18 +115,6 @@ export default function NovaMarcacao() {
         }
       })
   }
-  const getDatasIndisponiveis = () => {
-    axiosClient
-      .get('/marcacaos')
-      .then(({ data }) => {
-        const horasIndisponiveis = data.data.map((marcacao) => {
-          const dataHora = new Date(marcacao.data);
-          return dataHora.getHours() * 60 + dataHora.getMinutes();
-        });
-        setDatasIndisponiveis(horasIndisponiveis);
-      })
-      .catch(() => {});
-  };
 
   return (
     <>
@@ -137,7 +139,20 @@ export default function NovaMarcacao() {
               <br /><br />
               <h4 style={{ marginBottom: '10px' }}>Selecionar serviço:</h4>
               <select className='dropdown-servico' style={{ marginTop: '20px' }} onChange={(ev) => {
-                setMarcacao({ ...marcacao, servico: ev.target.value });
+                let custo = 0;
+                switch(ev.target.value){
+                  case 'Corte':
+                    custo = 10;
+                    break;
+                  case 'Barba':
+                    custo = 8;
+                    break;
+                  case 'Corte + Barba':
+                    custo = 12;
+                    break;
+                }
+
+                setMarcacao({ ...marcacao, servico: ev.target.value, custo });
                 getBarbeirosEspecialidade(ev.target.value);
               }}>
                 <option value="">Escolha uma opção</option>
@@ -161,29 +176,24 @@ export default function NovaMarcacao() {
             </div>
             <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px', textAlign: 'center' }}>
               <h4 style={{ marginBottom: '10px' }}>Selecionar data e hora:</h4>
-              <DatePicker
-  style={{ width: '300px' }}
-  className='dropdown-horario'
-  selected={dataHoraSelecionada}
-  onChange={(date) => mudarData(date)}
-  showTimeSelect
-  timeIntervals={30}
-  timeFormat='HH:mm'
-  dateFormat='dd/MM/yyyy HH:mm'
-  minDate={new Date()}
-  maxDate={new Date('2030-12-31')}
-  minTime={new Date().setHours(9, 0)}
-  maxTime={new Date().setHours(17, 30)}
-  filterDate={(date) => {
-    const day = date.getDay();
-    return day !== 6 && day !== 0;
-  }}
-  excludeTimes={datasIndisponiveis.map((hora) => {
-    const dataHoraBloqueada = new Date(dataHoraSelecionada.getFullYear(), dataHoraSelecionada.getMonth(), dataHoraSelecionada.getDate(), Math.floor(hora / 60), hora % 60);
-    return dataHoraBloqueada;
-  })}
-
-/>
+              <DatePicker 
+              style={{ width:'300px' }}
+              className='dropdown-horario'
+              selected={dataHoraSelecionada}
+              onChange={(date) => mudarData(date)}
+              showTimeSelect
+              timeIntervals={30}
+              timeFormat="HH:mm"
+              dateFormat="dd/MM/yyyy HH:mm"
+              minDate={new Date()}
+              maxDate={new Date('2030-12-31')}
+              minTime={new Date().setHours(9, 0)}
+              maxTime={new Date().setHours(17, 30)}
+              filterDate={(date) => {
+                const day = date.getDay();
+                return day !== 6 && day !== 0;
+              }}
+            />
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
               <button className='btn-marcacao'>Confirmar marcação</button>
