fb70d940963613b11ca4efc916268fef8ac5f62b
rafaelramalhoipvc <106443839+rafaelramalhoipvc@users.noreply.github.com>
2023-05-23 16:12:13 +0100

    signup e login

--- react/src/viewsAdmin/Signup.jsx
+++ react/src/viewsAdmin/Signup.jsx
@@ -1,68 +1,81 @@
-import {Link} from "react-router-dom";
-import {createRef, useState} from "react";
-import axiosClient from "../axios-client.js";
-import {useStateContext} from "../contexts/ContextProvider.jsx";
+import { Link } from 'react-router-dom';
+import { createRef, useState } from 'react';
+import axiosClient from '../axios-client.js';
+import { useStateContext } from '../contexts/ContextProvider.jsx';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
-  const nameRef = createRef()
-  const emailRef = createRef()
-  const passwordRef = createRef()
-  const passwordConfirmationRef = createRef()
-  const tipoRef = createRef()
-  const {setUser, setToken} = useStateContext()
-  const [errors, setErrors] = useState(null)
+  const nameRef = createRef();
+  const emailRef = createRef();
+  const passwordRef = createRef();
+  const passwordConfirmationRef = createRef();
+  const tipoRef = createRef();
+  const { setUser, setToken } = useStateContext();
+  const [errors, setErrors] = useState(null);
+  const navigate = useNavigate();
+  const {setNotification} = useStateContext()
 
   const onSubmit = ev => {
-    ev.preventDefault()
+    ev.preventDefault();
 
     const payload = {
-      
       name: nameRef.current.value,
       email: emailRef.current.value,
       password: passwordRef.current.value,
       password_confirmation: passwordConfirmationRef.current.value,
-      tipo: "Cliente",
-      
-    }
-    axiosClient.post('/signup', payload)
-      .then(({data}) => {
-        setUser(data.user)
-        setToken(data.token);
+      tipo: 'Cliente',
+    };
+
+    axiosClient
+      .post('/signup', payload)
+      .then(({ data }) => {
+        // Armazenar os dados do usuário e o token em variáveis locais
+        const { user, token } = data;
+
+        // Limpar os campos do formulário
+        nameRef.current.value = '';
+        emailRef.current.value = '';
+        passwordRef.current.value = '';
+        passwordConfirmationRef.current.value = '';
+
+        // Exibir mensagem de sucesso ou realizar outras ações necessárias
+        console.log('Registro bem-sucedido!');
+        setNotification('Marcação criada com sucesso');
+        navigate('/login');
+
       })
       .catch(err => {
         const response = err.response;
         if (response && response.status === 422) {
-          setErrors(response.data.errors)
+          setErrors(response.data.errors);
         }
-      })
-  }
+      });
+  };
 
   return (
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form onSubmit={onSubmit}>
-        <img
-            src="../src/img/IPCA-BarberShop.png"
-            alt="IPCA Barber Shop"
-            className="imagem-login"
-          />
+          <img src="../src/img/IPCA-BarberShop.png" alt="IPCA Barber Shop" className="imagem-login" />
           <h1 className="title">Criar conta</h1>
-          {errors &&
+          {errors && (
             <div className="alert">
               {Object.keys(errors).map(key => (
                 <p key={key}>{errors[key][0]}</p>
               ))}
             </div>
-          }
-          <input ref={nameRef} type="text" placeholder="Nome"/>
-          <input ref={emailRef} type="email" placeholder="Email"/>
-          <input ref={passwordRef} type="password" placeholder="Palavra-passe"/>
-          <input ref={passwordConfirmationRef} type="password" placeholder="Confirmar palavra-passe"/>
-        
+          )}
+          <input ref={nameRef} type="text" placeholder="Nome" />
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Palavra-passe" />
+          <input ref={passwordConfirmationRef} type="password" placeholder="Confirmar palavra-passe" />
+
           <button className="btn btn-block">Seguinte</button>
-          <p className="message">Já tem conta? <Link to="/login">Login</Link></p>
+          <p className="message">
+            Já tem conta? <Link to="/login">Login</Link>
+          </p>
         </form>
       </div>
     </div>
-  )
+  );
 }
