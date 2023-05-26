2028fa8f35a8a2a235018bd450f23f0b91fdba27
TomasFerreira66 <tomasff2002@gmail.com>
2023-05-26 03:15:15 +0100

    pedidos quantidade e nome

--- react/src/viewsAdmin/Pedidos.jsx
+++ react/src/viewsAdmin/Pedidos.jsx
@@ -7,68 +7,101 @@ export default function Pedidos() {
   const [pedidos, setPedidos] = useState([]);
   const [loading, setLoading] = useState(false);
   const { setNotification } = useStateContext();
-  const [users, setUsers] = useState({}); 
+  const [users, setUsers] = useState({});
+  const [produtos, setProdutos] = useState({});
   const { id } = useParams();
   const { user } = useStateContext();
 
-  const getUsers = (pedidos) => { 
-    const userIds = [...new Set(pedidos.map((carrinhos) => carrinhos.idCliente))]; 
+  const getUsers = (pedidos) => {
+    const userIds = [...new Set(pedidos.map((carrinho) => carrinho.idCliente))];
     const promises = userIds.map((id) => axiosClient.get(`/users/${id}`));
+    
     Promise.all(promises)
       .then((responses) => {
         const newUsers = {};
         responses.forEach((response) => {
           newUsers[response.data.id] = response.data.name;
+          console.log(response)
         });
+  
         setUsers(newUsers);
       })
       .catch(() => {
         setUsers({});
       });
   };
-  
+
+  const getProdutos = (pedidos) => {
+    const produtoIds = [...new Set(pedidos.map((carrinho) => carrinho.idProduto))];
+    const promises = produtoIds.map((id) => axiosClient.get(`/produtos/${id}`));
+    
+    Promise.all(promises)
+      .then((responses) => {
+        const newProdutos = {};
+        responses.forEach((response) => {
+          
+          newProdutos[response.data.data.id] = response.data.data.nome;
+          
+          console.log(response)
+        });
+        
+        setProdutos(newProdutos);
+        
+      })
+      .catch(() => {
+        setProdutos({});
+      });
+  };
+
   const getPedidos = () => {
     setLoading(true);
-    axiosClient.get('/carrinhos')
+    axiosClient
+      .get("/carrinhos")
       .then(({ data }) => {
         setLoading(false);
         setPedidos(data.data);
-        getUsers(data.data); 
       })
       .catch(() => {
         setLoading(false);
       });
   };
-  
+
   useEffect(() => {
     getPedidos();
   }, []);
 
+  useEffect(() => {
+    if (pedidos.length > 0) {
+      getUsers(pedidos);
+      getProdutos(pedidos);
+    }
+  }, [pedidos]);
 
   const distribuirPedidos = () => {
     setLoading(true);
-  
+
     // Get the IDs of the selected carrinhos
     const selectedCarrinhosIds = pedidos
-      .filter((carrinho) => carrinho.estado === 'Pago' && carrinho.selected)
+      .filter((carrinho) => carrinho.estado === "Pago" && carrinho.selected)
       .map((carrinho) => carrinho.id);
-  
+
     // Simulação de uma chamada assíncrona para atualizar o estado dos carrinhos
     setTimeout(() => {
       const pedidosAtualizados = pedidos.map((carrinho) => {
         if (selectedCarrinhosIds.includes(carrinho.id)) {
           // Update the estado directly in the current table
-          axiosClient.patch(`/carrinhos/${carrinho.id}`, { estado: 'Concluído' })
+          axiosClient
+            .patch(`/carrinhos/${carrinho.id}`, { estado: "Concluído" })
             .catch(() => {
               // Handle error if the update fails
-              setNotification('Failed to update estado.');
+              setNotification("Failed to update estado.");
             });
-  
-          return { ...carrinho, estado: 'Concluído' };
+
+          return { ...carrinho, estado: "Concluído" };
         }
         return carrinho;
       });
-  
+
       setPedidos(pedidosAtualizados);
       setLoading(false);
     }, 2000); // Tempo de espera simulado para demonstração
@@ -77,46 +110,85 @@ export default function Pedidos() {
   };
 
   const togglePedidoSelecionado = (id) => {
-  setPedidos(prevPedidos => prevPedidos.map(pedido => {
-    if (pedido.id === id) {
-      return {
-        ...pedido,
-        selected: !pedido.selected
-      };
-    }
-    return pedido;
-  }));
-};
+    setPedidos((prevPedidos) =>
+      prevPedidos.map((pedido) => {
+        if (pedido.id === id) {
+          return {
+            ...pedido,
+            selected: !pedido.selected,
+          };
+        }
+        return pedido;
+      })
+    );
+  };
 
   return (
-    <div style={{ marginLeft: '100px', marginRight: '100px' }}>
-      <div style={{ display: 'flex', justifyContent: "space-between", alignItems: "center" }}>
+    <div style={{ marginLeft: "100px", marginRight: "100px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <h2>Lista de pedidos</h2>
       </div>
       &nbsp;
-      <div className="card-container" style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gap: "10px" }}>
+      <div
+        className="card-container"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          gap: "10px",
+        }}
+      >
         {loading ? (
           <div>Loading...</div>
         ) : (
           pedidos
-            .filter((carrinhos) => carrinhos.estado === "Pago")
-            .map((carrinhos) => (
-              <div key={carrinhos.id} className="card animated fadeInDown" style={{ padding: "10px", borderRadius: "10px", position: "relative", height:'150px' }}>
-                <div style={{ marginBottom: "10px" }}>{`${carrinhos.id} - ${users[carrinhos.idCliente] || "-"}`}</div> 
-                <div style={{ fontSize: "18px", marginTop: "10px" }}>{carrinhos.morada}</div>
-                <div style={{ position: "absolute", bottom: "10px", right: "10px" }}>
-        <input
-          type="checkbox"
-          checked={carrinhos.selected}
-          onChange={() => togglePedidoSelecionado(carrinhos.id)}
-        />
-      </div>
+            .filter((carrinho) => carrinho.estado === "Pago")
+            .map((carrinho) => (
+              <div
+                key={carrinho.id}
+                className="card animated fadeInDown"
+                style={{
+                  padding: "10px",
+                  borderRadius: "10px",
+                  position: "relative",
+                  height: "150px",
+                }}
+              >
+                <div style={{ marginBottom: "10px" }}>
+                  {`${carrinho.id} - ${users[carrinho.idCliente] || "-"}`}
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                  {`${carrinho.quantidadePedida}x ${
+                    produtos[carrinho.idProduto] || "-"
+                  }`}
+                  
+                </div>
+                
+
+                <div style={{ fontSize: "18px", marginTop: "10px" }}>
+                  {carrinho.morada}
+                </div>
+                <div
+                  style={{ position: "absolute", bottom: "10px", right: "10px" }}
+                >
+                  <input
+                    type="checkbox"
+                    checked={carrinho.selected}
+                    onChange={() => togglePedidoSelecionado(carrinho.id)}
+                  />
+                </div>
               </div>
             ))
         )}
       </div>
-      <button onClick={distribuirPedidos} className="btn-login">Distribuir</button>
-
+      <button onClick={distribuirPedidos} className="btn-login">
+        Distribuir
+      </button>
     </div>
   );
 }
